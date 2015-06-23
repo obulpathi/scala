c659112b8ce5687893f26a2ccdcb858a0d91946c
Obulpathi <obulapathi.challa@rackspace.com>
2015-06-23 14:51:50 -0400

    Updated expressions

--- 03.Expressions/expressions.scala
+++ 03.Expressions/expressions.scala
@@ -1,3 +1,24 @@
+"hello" // a simple expression
+
+val s = "hello" // s is assigned the return value of expresison "hello"
+
+// code inside { } constitutes an expression block
+// the expresison block is evaluates and the resulting value
+// is assigned to v in the following example
+val v = {
+    var x: Int = 5
+    x + 10
+}
+println(v)
+
+// a statement does not return any value or it return Unit?
+// here the last statement is an assignment, which does not return any value
+val u = {
+    var x: Int = 10
+    x = x * 10 + 15
+}
+println(u)
+
 if (2 % 2 == 0) println("dsds")
 
 if (67 % 3 == 0) println("67 is a multiple of 3") else println("67 is not a multiple of 3")
