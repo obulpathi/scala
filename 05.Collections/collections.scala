c579102488c3a019445e88630688573b3bca4854
Obulpathi <obulapathi.challa@rackspace.com>
2015-06-24 14:14:08 -0400

    Collections

--- 05.Collections/collections.scala
+++ 05.Collections/collections.scala
@@ -1,3 +1,6 @@
+// The root of all iterable collections, Iterable , provides a common set of
+// methods for iterating through and manipulating collection data.
+
 val numbers = List(32, 95, 24, 21, 17)
 var total = 0;
 for (i <- numbers) { total += i }
@@ -9,9 +12,15 @@ println("Third element: " + numbers(2))
 
 println("Sum of numerrs in list: " + numbers + " is: " + total)
 
+// foreach, map, reduce
+numbers.foreach( (i: Int) => println(i * i))
+println(numbers.map((i: Int) => i * i))
+println(numbers.reduce((i: Int, j:Int) => i + j))
+
 val colors = List("red", "green", "blue")
 colors.foreach( (c: String) => println(c) )
 colors.map( (c: String) => c.size)
+// foreach vs map: output of map is a list too, output of foreach is not
 
 val sum = numbers.reduce( (a: Int, b: Int) => a + b )
 
